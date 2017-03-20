a4493c31c91c090410338d361c8fd969fd2688d9
Carlos sanz <Carlos sanz>
2017-03-20 17:31:43 +0100

    based Views

--- Shooppy/Scripts/Shoppy/products/urls.py
+++ Shooppy/Scripts/Shoppy/products/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.hola_mundo, name='hola'),
-	url(r'^product/(?P<pk>[0-9]+)/$', views.product_detail, name='product_detail'),
+	url(r'^$', views.ProductList.as_view(), name='hola'),
+	url(r'^product/(?P<pk>[0-9]+)/$', views.ProductDetail.as_view(), name='detail'),
 	url(r'^product/new', views.new_product, name='new_product'),
 ]
 
